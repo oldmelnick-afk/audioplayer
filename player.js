4515689617e7db8b396baaa775dee47ea08ff998
oldmelnick-afk <oldmelnick@gmail.com>
2025-11-05 22:46:42 +0300

    player.js

--- player.js
+++ player.js
@@ -1,7 +1,12 @@
 (() => {
   const PUBLIC_FOLDER_LINK = 'https://disk.yandex.ru/d/nQ8Hwia3_31HPA';
   const API_BASE = 'https://cloud-api.yandex.net/v1/disk/public';
-  const PROXY = 'https://proxy-h9qbqbjzc-oldmelnicks-projects.vercel.app/api/proxy?pk=';
+  // было (не работает с твоим прокси):
+// const PROXY = 'https://proxy-h9qbqbjzc-oldmelnicks-projects.vercel.app/api/proxy?pk=';
+
+// нужно:
+const PROXY = 'https://proxy-h9qbqbjzc-oldmelnicks-projects.vercel.app/api/proxy?u=';
+
   
   const audio    = document.getElementById('audio');
   const listEl   = document.getElementById('list');
