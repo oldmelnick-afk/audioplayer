769156573f5ed525f7c2f23861c62551ee2d8e06
oldmelnick-afk <oldmelnick@gmail.com>
2025-11-06 00:49:24 +0300

    player.js

--- player.js
+++ player.js
@@ -1,8 +1,8 @@
 (() => {
   const PUBLIC_FOLDER_LINK = 'https://disk.yandex.ru/d/nQ8Hwia3_31HPA';
   const API_BASE = 'https://cloud-api.yandex.net/v1/disk/public';
-  const PROXY = 'https://proxy-eqzxkm4mh-oldmelnicks-projects.vercel.app/api/proxy?u=';
-
+const PROXY = 'https://proxyjs-tau.vercel.app/api/proxy?u=';
+  
   const audio    = document.getElementById('audio');
   const listEl   = document.getElementById('list');
   const nowTitle = document.getElementById('nowTitle');
